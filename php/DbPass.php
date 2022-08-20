f88cc61a9328c914660ae5b45a9cf2848a859df3
iAmDaigoOhYeah <gcaoaad@gmail.com>
2022-08-20 13:28:14 +0900

    gitignore 見直し

---
+++ php/DbPass.php
@@ -0,0 +1,12 @@
+<?php
+// 本番環境
+define('HOST', '');
+define('DBNAME', '');
+define('DBUSER', '');
+define('DBPASS', '');
+
+// 検証環境
+// define('HOST', '');
+// define('DBNAME', '');
+// define('DBUSER', '');
+// define('DBPASS', '');
